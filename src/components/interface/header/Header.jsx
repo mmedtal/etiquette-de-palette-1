283a97caa924e885987cdb03594a91c593cd5012
liberation <62626976+mohammedlatrach@users.noreply.github.com>
2024-04-29 20:43:36 +0100

    ready to deploy

--- src/components/interface/header/Header.jsx
+++ src/components/interface/header/Header.jsx
@@ -13,8 +13,9 @@ export default function Header(props){
 
     return(
         <div className="flex justify-around">
-            <HeaderIcon text="Sélectionner" onClickDispatchActionsToReduxStore= {[{type:"SELECTIONNER"},{type:"AFFECTER_HAUTEUR",payload:0},{type:"AFFECTER_LARGEUR",payload:0} ]} >
+            <HeaderIcon disabled={true} text="Sélectionner" onClickDispatchActionsToReduxStore= {[{type:"SELECTIONNER"},{type:"AFFECTER_HAUTEUR",payload:0},{type:"AFFECTER_LARGEUR",payload:0} ]} >
                 {/* <TextFormat fontSize="large"/> */}
+                
                 <FontAwesomeIcon icon={faArrowPointer}/>
             </HeaderIcon>
             
@@ -23,14 +24,14 @@ export default function Header(props){
                 {/* <FontAwesomeIcon icon={faFont}/> */}
             </HeaderIcon>
 
-            <HeaderIcon text="Formes" onClickDispatchActionsToReduxStore= {[{type:"DESSINER_FORME"},{type:"AFFECTER_HAUTEUR",payload:0},{type:"AFFECTER_LARGEUR",payload:0} ]} >
+            <HeaderIcon disabled={true} text="Formes" onClickDispatchActionsToReduxStore= {[{type:"DESSINER_FORME"},{type:"AFFECTER_HAUTEUR",payload:0},{type:"AFFECTER_LARGEUR",payload:0} ]} >
                 {/* <div style={{height:"30px",width:"2.5px",backgroundColor:"grey",transform:"rotate(45deg)"}}>
                 </div> */}
                 {/* <FontAwesomeIcon icon={faSlash}/> */}
                 <ShapeLine fontSize="large"/>
             </HeaderIcon>
 
-            <HeaderIcon text="Barcode" onClickDispatchActionsToReduxStore= {[{type:"INSERER_BARCODE"},{type:"AFFECTER_HAUTEUR",payload:0},{type:"AFFECTER_LARGEUR",payload:0} ]} >        
+            <HeaderIcon disabled={true} text="Barcode" onClickDispatchActionsToReduxStore= {[{type:"INSERER_BARCODE"},{type:"AFFECTER_HAUTEUR",payload:0},{type:"AFFECTER_LARGEUR",payload:0} ]} >        
                 <FontAwesomeIcon icon={faBarcode}/>
             </HeaderIcon>
 
