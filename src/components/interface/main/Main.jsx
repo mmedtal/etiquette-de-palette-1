475b182011539534c9a94cadf36d10c57ffb3b9d
liberation <62626976+mohammedlatrach@users.noreply.github.com>
2024-05-04 10:57:07 +0100

    before adding zebra code tab

--- src/components/interface/main/Main.jsx
+++ src/components/interface/main/Main.jsx
@@ -32,30 +32,6 @@ export default function Main(props){
     const largeurPaletteControlledFromLeftAside = useSelector(state=>state.leftAsideControllersReducer.paletteLargeur)
     
 
-    
-    
-    // useEffect(()=>{
-    //     //console.log("i changed")
-    //     setHauteur(hauteurControlledFromLeftAside)
-    //     //console.log("hauteur ",hauteur)
-    // },[hauteurControlledFromLeftAside])
-
-    // useEffect(()=>{
-    //     //console.log("i changed")
-    //     setLargeur(largeurControlledFromLeftAside)
-    //     //console.log("hauteur ",hauteur)
-    // },[largeurControlledFromLeftAside])
-    /*
-    useEffect(()=>{
-        setHauteur(hauteur+hauteurControlledFromLeftAside)
-        console.log("hauteurControlledFromLeftAside",hauteurControlledFromLeftAside)
-    },[hauteurControlledFromLeftAside])
-
-    useEffect(()=>{
-        setLargeur(largeur+largeurControlledFromLeftAside)
-    },[largeurControlledFromLeftAside])
-    */
-
     function handleMainComponentClick(){
         dispatch({type:"SET_LEFT_ASIDE_CLICKED",payload:false})
     }
@@ -64,37 +40,11 @@ export default function Main(props){
         <div className="flex-col"  
             onClick={handleMainComponentClick}
         >
-        {/* <button onClick={()=>console.log("hauteur ",hauteur)}>ccc</button> */}
 
             <div className="header h-10">
 
             </div>
-            {/* <Card variant="outlined" className="w-5/6 h-96" elevation={12}>
-                <CardHeader title="Etiquette Test" className="text-center"/>                    
-                <hr />
-                <CardContent>
-                    <TextField variant="standard"/>
-                    <TextField variant="standard" style={{marginLeft:"-20px"}}/>
-                    <div className="flex justify-around">
-                        <div>
-                            zone text 1
-                        </div>
-
-                        <div>
-                            zone text 2
-                        </div>
-                    </div>
-                </CardContent>
-                <hr />
-                <br />
-                <br />
-                <br />
-                <div className="text-center">
-                    le reste..
-                </div>
-            </Card> */}
             <div className="flex justify-center">
-                {/* <PaletteWorkZone height={hauteurControlledFromLeftAside+450} width={largeurControlledFromLeftAside+600}/> hadi ça passe */}
                 <PaletteWorkZone height={hauteurPaletteControlledFromLeftAside} width={largeurPaletteControlledFromLeftAside}/>
             </div>
         </div>
